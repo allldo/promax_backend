abb7ffa88ce6fcf947eb6897163b0da466ae32ae
Allldo <dotan2.maks@mail.ru>
2024-11-17 23:16:02 +0500

    admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from orders.models import ServiceOrder
+from orders.models import ServiceOrder, ProductOrder, ExpressCalc
 
 admin.site.register(ServiceOrder)
+admin.site.register(ProductOrder)
+admin.site.register(ExpressCalc)
