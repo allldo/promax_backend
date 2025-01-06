42e6dede05916408899b8f17a94a8f96cb232a78
Allldo <dotan2.maks@mail.ru>
2025-01-06 14:44:07 +0500

    shop admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from shop.models import Product, SubCategory, Category, Image
+from shop.models import Product, SubCategory, Category, Image, Attachment
 
+admin.site.register(Attachment)
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(SubCategory)
